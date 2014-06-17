ea92486e7fdf6b249e335ad45f0f8ccc0b4bbcec
borand <andrzej@borowiec.ca>
2014-06-17 16:19:29 -0400

    submission process

--- rtweb/daq_irq.py
+++ rtweb/daq_irq.py
@@ -1,14 +1,41 @@
+"""daq_irq.py -
+
+module used to listen for new data on the redis channel and submit the data to sensoredweb
+
+Usage:
+  daq_irq.py test [--dev=DEV | --submit_to=SUBMIT_TO | --redishost=REDISHOST]
+  daq_irq.py run  [--dev=DEV | --submit_to=SUBMIT_TO | --redishost=REDISHOST]
+  daq_irq.py (-h | --help)
+
+Options:
+  -h, --help
+  --dev=DEV              [default: /dev/ttyUSB0]
+  --submit_to=SUBMIT_TO  [default: 192.168.1.10]
+  --redishost=REDISHOST  [default: 192.168.1.10]
+
+"""
+
 import threading
-import click
 from redis import Redis
 from rq import Queue
 from logbook import Logger
 from datastore import submit
+from docopt import docopt
 import simplejson as sjson
 import datetime
 import time
 
 from common import get_host_ip
+
+##########################################################################################
+# Define special processing functions for various sensor data
+def process_power_meter_data(data):
+    return round(3600.0/((pow(2,16)*data[2] + data[3])/16e6*1024))
+
+def process_default(data):
+    return data
+
+ProcessingFunctions = {'irq_0' : process_power_meter_data}
 
 ##########################################################################################
 class IrqSubmit(threading.Thread):
@@ -57,8 +84,27 @@ class IrqSubmit(threading.Thread):
             if item['data'] == "ERROR_TEST":
                 self.redis.publish('error', __name__ + ": ERROR_TEST")
             else:
-                self.process_message(item)
+                self.print_message(item)
+                #self.process_message(item)
+
         self.Log.debug('end of run()')
+
+    def print_message(self, item):
+        try:
+            msg         = sjson.loads(item['data'])
+            device_data = msg['MSG']['data']
+            cmd         = msg['MSG']['cmd']
+            info_string = "{:20s} | {:10s} : {:s}".format(msg['FROM'],cmd,str(device_data))
+            print(info_string)
+
+            processing_function = ProcessingFunctions.get(cmd,process_default)
+            val    = processing_function(device_data)
+
+            print("Final dataset : sn = {:s}, val = {:f}".format(cmd, val))
+
+
+        except Exception as E:
+            self.Log.error(E.message)
 
     def process_message(self, item):
         try:
@@ -98,15 +144,11 @@ class IrqSubmit(threading.Thread):
             self.Log.error(E.message)
             self.redis.publish('error', E.message)
 
-@click.command()
-@click.option('--channel', default='irq')
-@click.option('--host', default=get_host_ip())
-@click.option('--submit_to', default=get_host_ip())
-
 def StartIqrSubmit(channel, host, submit_to):
     print"StartIqrSubmit(%s, %s, %s)" % (channel, host, submit_to)
     try:
         I = IrqSubmit(channel=channel, host=host, submit_to=submit_to);
+        print("===============================================")
         while True:
             pass
     except KeyboardInterrupt:
@@ -117,4 +159,14 @@ def StartIqrSubmit(channel, host, submit_to):
 
 ##########################################################################################
 if __name__ == "__main__":
-    StartIqrSubmit()
+    arguments = docopt(__doc__, version='Naval Fate 2.0')
+    print("===============================================")
+    print(arguments)
+
+    dev = arguments['--dev']
+
+    if arguments['run']:
+        channel   = 'rtweb'
+        host      = get_host_ip()
+        submit_to = get_host_ip()
+        StartIqrSubmit(channel, host, submit_to)
